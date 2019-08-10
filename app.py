ff341e21f4fb49510f05142d6dbc54cc7a7bc753
koma <iloveyou80703@gmail.com>
2019-08-10 14:49:35 +0800

    make it better

--- app.py
+++ app.py
@@ -41,23 +41,23 @@ def callback():
 
 
 # 處理訊息
-import jieba.posseg as pseg
+#import jieba.posseg as pseg
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #拆解名詞（物品）
-    qst=event.message.text
-    reply=""
-    cuts = pseg.lcut(qst)
+    #qst=event.message.text
+    #reply=""
+    #cuts = pseg.lcut(qst)
 
-    for cut in cuts:
-        if(cut.flag =='n'):
-            reply = cut.word
+    #for cut in cuts:
+        #if(cut.flag =='n'):
+            #reply = cut.word
 
     ＃回傳訊息
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="ahah"))
+        TextSendMessage(text='ahah'))
 
 
 if __name__ == "__main__":
