56bf5f33187b4f71b786680b790a4b69f2f4b80b
koma <iloveyou80703@gmail.com>
2019-08-10 14:55:53 +0800

    make it better

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='hello word'))
 
 if __name__ == "__main__":
     app.run()
