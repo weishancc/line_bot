e01305b1cf75786197c7ae1e650d1343aecbbf98
iloveyou80703@gmail.com <iloveyou80703@gmail.com>
2019-08-11 13:52:23 +0800

    make it better

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def handle_message(event):
             for data in datas:
                 line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text = type(data)))
+                    TextSendMessage(text = '123'))
 
     if(not find):
         line_bot_api.reply_message(
