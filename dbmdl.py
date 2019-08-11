02a3b5703e1246f2560cf40f308cde4460570c8b
iloveyou80703@gmail.com <iloveyou80703@gmail.com>
2019-08-11 12:42:00 +0800

    make it better

--- dbmdl.py
+++ dbmdl.py
@@ -3,24 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-#設定資料庫位置/並建立app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'tlsyabrsdacvzb:0052cd379921a09ed41de5c9ef69a3c045b6de738c120fa6ec2f4291f57cf7e3@ec2-174-129-227-205.compute-1.amazonaws.com:5432/d8aekdhm679t9f'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tlsyabrsdacvzb:0052cd379921a09ed41de5c9ef69a3c045b6de738c120fa6ec2f4291f57cf7e3@ec2-174-129-227-205.compute-1.amazonaws.com:5432/d8aekdhm679t9f'
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#設定app
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-#建立資料表欄位
 class ItemInfo(db.Model):
     __tablename__ = 'ItemInfo'
     name = db.Column(db.String(20), primary_key=True)
     price = db.Column(db.Integer)
     stock = db.Column(db.Boolean)
     category = db.Column(db.String(5))
-
+    
     def __init__(self
                  , name
                  , price
@@ -33,5 +32,5 @@ class ItemInfo(db.Model):
         self.category = category
 
 
-#if __name__ == '__main__':
-#    manager.run()
+if __name__ == '__main__':
+    manager.run()
