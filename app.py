c09e8fe8c2890df534b5b223229248156fd4f952
iloveyou80703@gmail.com <iloveyou80703@gmail.com>
2019-08-11 12:38:38 +0800

    make it better

--- app.py
+++ app.py
@@ -60,11 +60,15 @@ def handle_message(event):
             find = True
 
             #進入進料找資料
-            datas = ItemInfo.query.filter_by(name = cut).first()
-            #if(datas is not None):
+            datas = ItemInfo.query.filter_by(name = '健身房').first()
             line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text = ('商品名稱： ' + datas.name + '\n價格： ' + datas.price + '\n庫存： ' + str(datas.stock)))
+            TextSendMessage(text = datas.name)
+
+            #if(datas is not None):
+            #line_bot_api.reply_message(
+            #event.reply_token,
+            #TextSendMessage(text = ('商品名稱： ' + datas.name + '\n價格： ' + datas.price + '\n庫存： ' + str(datas.stock)))
     if(not find):
         line_bot_api.reply_message(
             event.reply_token,
