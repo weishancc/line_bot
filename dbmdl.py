2099264f7598ae7c5d73e64ffffea4a006a7fb1a
iloveyou80703@gmail.com <iloveyou80703@gmail.com>
2019-08-11 14:15:10 +0800

    make it better

--- dbmdl.py
+++ dbmdl.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tlsyabrsdacvzb:0052cd379921a09ed41de5c9ef69a3c045b6de738c120fa6ec2f4291f57cf7e3@ec2-174-129-227-205.compute-1.amazonaws.com:5432/d8aekdhm679t9f'
 
@@ -19,7 +19,7 @@ class ItemInfo(db.Model):
     price = db.Column(db.Integer)
     stock = db.Column(db.Boolean)
     category = db.Column(db.String(5))
-    
+
     def __init__(self
                  , name
                  , price
