b68fdedb9328f71f73165e20c100d7d8772b8784
iloveyou80703@gmail.com <iloveyou80703@gmail.com>
2019-08-11 13:32:07 +0800

    make it better

--- app.py
+++ app.py
@@ -60,10 +60,11 @@ def handle_message(event):
             find = True
 
             #進入進料找資料
-            datas = ItemInfo.query.filter_by(name = cut).first()
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text = '@@'))
+            datas = ItemInfo.query.filter(ItemInfo.category == 'bn').all()
+            for data in datas:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text = data.name))
 
     if(not find):
         line_bot_api.reply_message(
